738eaa09064822ba05ac8dace612cf5e40aba7e5
SirMeowingtons5 <34872618+SirMeowingtons5@users.noreply.github.com>
2021-08-01 23:34:18 +0300

    core/data: added repository implementation and local data source

---
+++ app/src/main/java/com/sirmeowingtons5/interviewapp/data/repository/InterviewRepositoryImpl.kt
@@ -0,0 +1,18 @@
+package com.sirmeowingtons5.interviewapp.data.repository
+
+import com.sirmeowingtons5.interviewapp.data.datasource.InterviewDataSource
+import com.sirmeowingtons5.interviewapp.domain.entity.common.ID
+import com.sirmeowingtons5.interviewapp.domain.entity.interview.Interview
+import com.sirmeowingtons5.interviewapp.domain.repository.InterviewRepository
+
+class InterviewRepositoryImpl(
+    private val dataSource: InterviewDataSource,
+) : InterviewRepository {
+
+    override fun get(id: ID): Interview =
+        dataSource.getById(id)
+
+    override fun save(interview: Interview) {
+        dataSource.save(interview)
+    }
+}
